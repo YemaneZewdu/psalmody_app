88b0dcbe545a52e805f49914d2e174c121e762d6
YemaneZewdu <yemane18berhan@gmail.com>
2019-10-15 17:44:59 -0400

    Web view for launching pdf books urls

--- lib/view/books_list_screen.dart
+++ lib/view/books_list_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class BooksListScreen extends StatelessWidget {
-  final _books = [{'title':'Book 1', 'description':"description 1"} , {'title':'Book 2', 'description':"description 2"}, {'title':'Book 3', 'description':"description 3"}];
-
+  final _books = [
+    {'title': 'Yezewtir Gitsawe PDF', 'description': 'Yezewetir kidase mawcha', 'url': 'https://drive.google.com/file/d/14FKmgnUe7jMHcZZMsswvuXiZHP-ZEP6Y/view?usp=sharing' },
+    {'title': 'Yetewsak Gitsawe PDF', 'description': 'Letewsak bealat kidase mawcha', 'url': 'https://drive.google.com/file/d/1b4i7DAVmF7ncGHL0G7Cd53cU6mtSPJjp/view?usp=sharing'},
+    {'title': 'Yesenbet Gitsawe PDF', 'description': 'Yesenbet kidase mawcha', 'url': 'https://drive.google.com/file/d/1-IwFZKu3tvmoOb34HCZ8BPnnigQl3sLn/view?usp=sharing'}
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,28 +14,58 @@ class BooksListScreen extends StatelessWidget {
       body: new ListView.builder(
         itemCount: _books.length,
         itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: EdgeInsets.only(bottom: 10.0),
-            child: Card(
-              color: Colors.white,
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 15.0),
-                child: ListTile(
-                  title: Text(
-                    _books[index]['title'],
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      height: 1.6,
+          return GestureDetector(
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return LaunchUrl(title: _books[index]['title'], url: _books[index]['url']);
+                },
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 10.0),
+              child: Card(
+                color: Colors.white,
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 15.0),
+                  child: ListTile(
+                    title: Text(
+                      _books[index]['title'],
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        height: 1.6,
+                      ),
                     ),
-                  ),
-                  subtitle: Text(
-                    _books[index]['description'],
+                    subtitle: Text(
+                      _books[index]['description'],
+                    ),
                   ),
                 ),
               ),
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class LaunchUrl extends StatelessWidget {
+  final String title;
+  final String url;
+
+  LaunchUrl({Key key, this.title, this.url}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      withLocalStorage: true,
+      url: url,
+      withZoom: true,
+      hidden: true,
+      appBar: AppBar(
+        title: Text(title),
       ),
     );
   }
