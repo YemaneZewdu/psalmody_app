887eb0e53128f7d9a43870e865fc884d3f8e74a0
Yemane Zewdu <yemanezewdu@Yemanes-MacBook-Pro.local>
2021-06-06 15:16:29 -0400

    Fixed app migration issue and added ios files

--- android/app/src/main/kotlin/com/example/psalmody/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/psalmody/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.psalmody
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
+//import android.os.Bundle
+//import io.flutter.app.FlutterActivity
+//import io.flutter.plugins.GeneratedPluginRegistrant
+mport io.flutter.embedding.android.FlutterActivity
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+//  override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    GeneratedPluginRegistrant.registerWith(this)
+//  }
 }
